c0e8e9c33b6478a545c7e8cddedc9386545341ab
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-12-02 23:11:17 +0100

    day 02 part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let input = include_str!("../../aoc-2025-inputs/day-02/input.txt");
     let now = std::time::Instant::now();
     dbg!(part_1(input));
-    // dbg!(part_2(input));
+    dbg!(part_2(input));
     println!("Time: {:?}", now.elapsed());
 }
 
@@ -32,7 +32,35 @@ fn part_1(input: &str) -> u64 {
 }
 
 fn part_2(input: &str) -> u64 {
-    todo!()
+    let res = input.trim_end().split(',').fold(0, |acc, range_str| {
+        let range_min = range_str.split('-').next().unwrap().parse::<i64>().unwrap();
+        let range_max = range_str.split('-').nth(1).unwrap().parse::<i64>().unwrap();
+        let mut invalid_id = 0;
+
+        for i in range_min..=range_max {
+            let i_string = i.to_string();
+            let i_vec = i_string.chars().collect::<Vec<char>>();
+            let max_size = (i_vec.len() / 2).max(1);
+
+            for chunk_size in 1..=max_size {
+                if chunk_size > 0 {
+                    let num_iter = i_vec.chunks_exact(chunk_size);
+                    if num_iter.remainder().is_empty() && (i_vec.len() > 1) {
+                        let num_vec = num_iter.map(|x| x.iter().collect::<String>().parse::<i64>().unwrap()).collect::<Vec<i64>>();
+                        let repeat_val = num_vec[0];
+                        if num_vec.iter().all(|x| *x == repeat_val) {
+                            invalid_id += i;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        acc + invalid_id
+    });
+
+    res.try_into().unwrap()
 }
 
 #[cfg(test)]
@@ -53,7 +81,13 @@ mod tests_day_02 {
 
     #[test]
     fn test_part_2() {
+        let input = "1-19";
+        assert_eq!(part_2(input), 11);
+
+        let input = "1188511880-1188511890";
+        assert_eq!(part_2(input), 1188511885);
+
         let input = include_str!("../../aoc-2025-inputs/day-02/test.txt");
-        // assert_eq!(part_2(input), 0);
+        assert_eq!(part_2(input), 4174379265);
     }
 }
