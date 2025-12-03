f027c4ebcb0894754ebd76c38548c43a00b4b34c
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-12-03 08:28:05 +0100

    day 03 part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -22,7 +22,22 @@ fn part_1(input: &str) -> u64 {
 }
 
 fn part_2(input: &str) -> u64 {
-    0
+    input.lines().fold(0, |acc, line| {
+        let digit_vec = line.chars().enumerate().map(|(i, char)| (i, char.to_digit(10).unwrap())).collect::<Vec<(usize, u32)>>();
+        let mut max_joltage : u64 = 0;
+        let mut left_border : usize = 0;
+        for i in 0..12 {
+            let mut current_digit: (usize, u32) = (left_border, 0);
+            for digit in digit_vec[left_border..(digit_vec.len()-(11-i))].iter() {
+                if digit.1 > current_digit.1 {
+                    current_digit = *digit;
+                }
+            }
+            max_joltage = max_joltage * 10 + (current_digit.1 as u64);
+            left_border = current_digit.0 + 1;
+        }
+        acc + max_joltage
+    })
 }
 
 #[cfg(test)]
@@ -38,6 +53,6 @@ mod tests_day_03 {
     #[test]
     fn test_part_2() {
         let input = include_str!("../../aoc-2025-inputs/day-03/test.txt");
-        assert_eq!(part_2(input), 0);
+        assert_eq!(part_2(input), 3121910778619);
     }
 }
