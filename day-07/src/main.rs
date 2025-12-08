01f895a18a7ffe85b488b3ca09414427e577465b
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-12-08 23:49:57 +0100

    day 07 part 1

---
+++ day-07/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("../../aoc-2025-inputs/day-07/input.txt");
+    let now = std::time::Instant::now();
+    dbg!(part_1(input));
+    dbg!(part_2(input));
+    println!("Time: {:?}", now.elapsed());
+}
+
+fn part_1(input: &str) -> u64 {
+    let mut lines_iter = input.lines().map(|line| line.chars().collect::<Vec<char>>());
+    let first_beam = lines_iter.next().unwrap().iter().position(|&x| x == 'S').unwrap();
+
+    let mut split = 0;
+    let mut beams: HashSet<usize> = HashSet::new();
+    beams.insert(first_beam);
+    for line in lines_iter {
+        let mut new_beams: HashSet<usize> = HashSet::new();
+        for &beam in beams.iter() {
+            if line[beam] == '^' {
+                new_beams.insert(beam-1);
+                new_beams.insert(beam+1);
+                split += 1;
+            } else {
+                new_beams.insert(beam);
+            }
+        }
+
+        beams = new_beams;
+    }    
+
+    split
+}
+
+fn part_2(input: &str) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests_day_07 {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = include_str!("../../aoc-2025-inputs/day-07/test.txt");
+        assert_eq!(part_1(input), 21);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = include_str!("../../aoc-2025-inputs/day-07/test.txt");
+        assert_eq!(part_2(input), 0);
+    }
+}
