8b1813cb8ee18589eb7cb7f244de3abc0001a6ee
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-12-11 08:19:58 +0100

    day 11 part 1

---
+++ day-10/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let input = include_str!("../../aoc-2025-inputs/day-10/input.txt");
+    let now = std::time::Instant::now();
+    dbg!(part_1(input));
+    dbg!(part_2(input));
+    println!("Time: {:?}", now.elapsed());
+}
+
+fn part_1(input: &str) -> u64 {
+    todo!()
+}
+
+fn part_2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests_day_10 {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = include_str!("../../aoc-2025-inputs/day-10/test.txt");
+        assert_eq!(part_1(input), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = include_str!("../../aoc-2025-inputs/day-10/test.txt");
+        assert_eq!(part_2(input), 0);
+    }
+}
---
+++ day-11/src/main.rs
@@ -0,0 +1,61 @@
+use std::collections::{HashMap, VecDeque};
+
+fn main() {
+    let input = include_str!("../../aoc-2025-inputs/day-11/input.txt");
+    let now = std::time::Instant::now();
+    dbg!(part_1(input));
+    dbg!(part_2(input));
+    println!("Time: {:?}", now.elapsed());
+}
+
+fn part_1(input: &str) -> u64 {
+    let mut nodes_list: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in input.lines() {
+        let mut next = Vec::new();
+
+        for connections in line[5..].split(' ') {
+            next.push(connections);
+        }
+
+        nodes_list.insert(line[0..3].as_ref(), next);
+    }
+
+    let mut path_list: VecDeque<&str> = VecDeque::new();
+    path_list.push_back("you");
+    let mut path_cnt = 0;
+    while !path_list.is_empty() {
+        let node = path_list.pop_front().unwrap();
+
+        if node == "out" {
+            path_cnt += 1;
+        } else if let Some(next) = nodes_list.get(node) {
+            for &entry in next.iter() {
+                path_list.push_back(entry);
+            }
+        }
+    }
+
+    path_cnt
+}
+
+fn part_2(input: &str) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests_day_11 {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let input = include_str!("../../aoc-2025-inputs/day-11/test.txt");
+        assert_eq!(part_1(input), 5);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input = include_str!("../../aoc-2025-inputs/day-11/test.txt");
+        assert_eq!(part_2(input), 0);
+    }
+}
