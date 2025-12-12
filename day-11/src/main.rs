10b62ea50affcf4268ac952ab7d1cae7076d2c4f
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2025-12-12 20:12:11 +0100

    fix day 11 part 2 with DFS cached

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 fn main() {
     let input = include_str!("../../aoc-2025-inputs/day-11/input.txt");
@@ -39,13 +39,32 @@ fn part_1(input: &str) -> u64 {
     path_cnt
 }
 
-struct State<'a> {
-    visits: HashSet<&'a str>,
-    current_node: &'a str,
+fn count_path_to_out<'a>(graph: &HashMap<&'a str, Vec<&'a str>>, start_node: &'a str, mut has_visit_fft: bool, mut has_visit_dac: bool, cache: &mut HashMap<(&'a str, bool, bool), u64>) -> u64 {
+    if start_node == "fft" {
+        has_visit_fft = true;
+    } else if start_node == "dac" {
+        has_visit_dac = true;
+    }
+
+    if start_node == "out" {
+        if has_visit_fft && has_visit_dac { 1 } else { 0 }
+    } else if let Some(cached_val) = cache.get(&(start_node, has_visit_fft, has_visit_dac)) {
+        *cached_val
+    } else {
+
+        let res = graph.get(start_node).unwrap().iter().fold(0, |acc, &child| {
+            acc + count_path_to_out(graph, child, has_visit_fft, has_visit_dac, cache)
+        });
+
+        cache.insert((start_node, has_visit_fft, has_visit_dac), res);
+
+        res
+    }
 }
 
 fn part_2(input: &str) -> u64 {
     let mut nodes_list: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut cache: HashMap<(&str, bool, bool), u64> = HashMap::new();
 
     for line in input.lines() {
         let mut next = Vec::new();
@@ -57,37 +76,8 @@ fn part_2(input: &str) -> u64 {
         nodes_list.insert(line[0..3].as_ref(), next);
     }
 
-    let mut path_list: VecDeque<State> = VecDeque::new();
-    path_list.push_back(State {
-        visits: {
-            let mut hash = HashSet::new();
-            hash.insert("svr");
-            hash
-        },
-        current_node: "svr",
-    });
-    let mut path_cnt = 0;
-    while !path_list.is_empty() {
-        let mut node = path_list.pop_front().unwrap();
 
-        if node.current_node == "out" {
-            if node.visits.contains("fft") && node.visits.contains("dac") {
-                path_cnt += 1;
-            }
-        } else if let Some(next) = nodes_list.get(node.current_node) {
-            for &entry in next.iter() {
-                if !node.visits.contains(entry) {
-                    node.visits.insert(entry);
-                    path_list.push_back(State {
-                        visits: node.visits.clone(),
-                        current_node: entry,
-                    });
-                }
-            }
-        }
-    }
-
-    path_cnt
+    count_path_to_out(&nodes_list, "svr", false, false, &mut cache)
 }
 
 #[cfg(test)]
